db63506b20b974bd0f0d0ea4ae1e40fcac37b67a
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-06-29 19:55:09 -0400

    useEffect and cleanUp function had to write a state variable to keep track of whether the component was still mounted. But I think I can still see something running in the background because the timer flickers but idk, passed tests though.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  /*
+  function cleanup() {
+    if(timeRemaining ===0){
+      setTimeRemaining(10);
+      onAnswered(false)
+    }
+  }
+
+  useEffect((()=>{
+    setTimeout(()=>{
+      setTimeRemaining(
+        (x)=> x = timeRemaining - 1)}
+    , 1000)
+  return cleanup})
+  , [timeRemaining]);
+  */
+
+  const [isMounted, setIsMounted] = useState(true);
+
+  function cleanup(time) {
+    clearTimeout(time);
+    if(time === 0){
+    //clearTimeout(time);
+    setTimeRemaining(()=>10);
+    onAnswered(false);
+    }
+    if(!question){
+      setIsMounted(false);
+    }
+    return isMounted;
+  }
+
+  useEffect((()=>{
+    setTimeout(()=>{
+      setTimeRemaining(
+        (x)=> x = timeRemaining - 1)}
+    , 1000)
+  return cleanup(timeRemaining)})
+  , [timeRemaining, question]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
-
+// HOW DOES THIS WORK???
   const { id, prompt, answers, correctIndex } = question;
 
   return (
